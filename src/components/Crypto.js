e33519353d4e71a550a419daa7a91653ca5396e7
Pavan <101943293+Pavana004@users.noreply.github.com>
2023-05-19 20:26:49 +0530

    final

--- src/components/Crypto.js
+++ src/components/Crypto.js
@@ -24,8 +24,7 @@ const Crypto = () => {
     const fetchData = async () => {
         try {
             setLoading(true);
-            const res = await axios.get("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en")
-            console.log(res.data);
+            const res = await axios.get("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en");
             setCrytData(res.data);
             setFilterData(res.data);
             setLoading(false);
